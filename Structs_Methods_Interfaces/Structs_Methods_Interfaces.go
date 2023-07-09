c3ba3f6a29adb6c38d9f8c732018f178e450b76f
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-09 18:00:17 +0630

    structs methods interfaces readme added

--- Structs_Methods_Interfaces/Structs_Methods_Interfaces.go
+++ Structs_Methods_Interfaces/Structs_Methods_Interfaces.go
@@ -1,5 +1,9 @@
 package Structs_Methods_Interfaces
 
+import (
+	"fmt"
+)
+
 var Tr_eiphyo goethe_teacher
 var Tr_helen british_council_teacher
 
@@ -14,4 +18,19 @@ func Structs_Methods_Interfaces() {
 		location: "Yangon",
 		age:      28,
 	}
+
+	one_time_struct := struct {
+		official_name string
+		like_it       bool
+	}{
+		official_name: "Anonymous structs or something like that",
+		like_it:       true,
+	}
+	fmt.Println("concept:", one_time_struct.official_name)
+	if one_time_struct.like_it {
+		fmt.Println("I kinda like them")
+	} else {
+		fmt.Println("I kinda don't like them")
+	}
+
 }
---
+++ Structs_Methods_Interfaces/readme.md
@@ -0,0 +1,4 @@
+# Structs, Methods, and Interfaces (combined)
+
+## Other Findings
+1. You can use the & with an already defined struct to create a new instance of that particular struct and obtain the pointer to that instance.
