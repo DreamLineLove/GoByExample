c553365f04b1977a0cd1fcbb56d02b5405eaf18a
DreamLineLove <zwenyanzaw@protonmail.com>
2023-07-09 18:46:11 +0630

    experimented with errorF: both %w and %v

--- Errors/Errors.go
+++ Errors/Errors.go
@@ -8,4 +8,24 @@ import (
 func Errors() {
 	simpleError := errors.New("This should return an error")
 	fmt.Println(simpleError)
+
+	if message, ok := simpleError.(error); ok {
+		fmt.Println("This was an error indeed!\n\tMessage:", message)
+	}
+
+	simpleErrorf := fmt.Errorf("this is a simple Errorf without any contained errors")
+	fmt.Println(simpleErrorf)
+
+	err1 := errors.New("w1")
+	errorF := fmt.Errorf("outer, and inner: %v", err1)
+	fmt.Println(errorF)
+
+	// The following logic returns nil.
+	inner := errors.Unwrap(errorF)
+	fmt.Println(inner)
+
+	err1 = errors.New("w1 again")
+	errorF = fmt.Errorf("outer, and inner: %w", err1)
+	inner = errors.Unwrap(errorF)
+	fmt.Println(inner)
 }
